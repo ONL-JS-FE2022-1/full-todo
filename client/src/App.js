712641bc6db5b38dc0712f814290a8812a66b5ce
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-02-28 20:52:03 +0200

    Add pages, components, router + server model, controllers

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,16 @@
+import React, {useReducer} from 'react';
 
+const reducer = (state, action) => {
+  return state;
+}
 
 function App() {
+
+  const [state, dispatch] = useReducer({
+    user: null,
+    tasks: []
+  })
+
   return (
     <div>App</div>
   );
