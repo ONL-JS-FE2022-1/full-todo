67966a4342c27d6cad82c1b24f81c5fe3f4a0ac2
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-03-01 20:17:26 +0200

    Registration done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,7 @@
 import React, {useReducer} from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import TodoPage from './pages/TodoPage';
 
 const reducer = (state, action) => {
   return state;
@@ -12,7 +15,14 @@ function App() {
   })
 
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        {/* localhost:3000/ -> Home component */}
+        <Route path="/tasks/" element={<TodoPage />}/>
+        {/* localhost:3000/tasks/ -> TodoPage component */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,34 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import {registerUser} from '../api/index';
 import SignIn from '../components/SignIn';
 import SignUp from '../components/SignUp';
 
 const Home = () => {
     const [state, setState] = useState(true);
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        if(data) {
+            registerUser(data)
+            .then(result => {
+                console.log(result)
+            })
+            .catch(err => {
+                setError(err);
+            })
+        }
+    }, [data])
 
     const buttonHandler = () => {
         setState(state => !state);
     }
 
-    const textButton = state ? "SignUp" : "SignIn";
+    const getData = (userData) => {
+        setData(userData);
+    }
+
+    const textButton = state ? "SignIn" : "SignUp";
 
     return (
         <>
@@ -18,8 +37,9 @@ const Home = () => {
         </header>
 
         <main>
-            {state ? <SignIn/> : <SignUp/>}
+            {state ? <SignUp sendData={getData} /> : <SignIn sendData={getData} />}
         </main>
+        {error && <div>{error}</div>}
         </>
     );
 }
