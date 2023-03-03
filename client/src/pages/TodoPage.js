4b9db12d77c6ed17ce74f97c9137c110005e51af
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-03-03 20:56:15 +0200

    Get tasks done

--- client/src/pages/TodoPage.js
+++ client/src/pages/TodoPage.js
@@ -1,7 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import TodoList from '../components/TodoList';
-import { getTasks } from '../api/taskApi';
+import { getTasks, createTask } from '../api/taskApi';
 import { useNavigate } from 'react-router-dom';
+import ToDoForm from '../components/TodoForm';
 
 const TodoPage = (props) => {
     const [todos, setTodos] = useState([]);
@@ -20,9 +21,21 @@ const TodoPage = (props) => {
         })
     }, []);
 
+    const getNewTd = (data) => {
+        createTask({
+            authorId: props.user._id,
+            status: 'new',
+            ...data
+        }).then(({data: createdTask}) => {
+            const newTodo = [...todos, createdTask];
+            setTodos(newTodo);
+        })
+    }
+
     return (
         <div>
             <h1>ToDo List</h1>
+            <ToDoForm sendData={getNewTd} />
             <TodoList todos={todos} />
         </div>
     );
