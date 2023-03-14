c76d3212c38bf854ad0f2e9e3f9962ea28f23eb9
Roman Ratyshnyi <freshjs.ofe@gmail.com>
2023-03-14 19:46:37 +0200

    Remebering useReducer (task resolve)

--- client/src/Counter.js
+++ client/src/Counter.js
@@ -1,19 +1,43 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
+
+const reducer = (state, action) => {
+    switch(action.type) {
+        case 'COUNTER_PLUS': {
+            return {
+                counter: state.counter + 1
+            }
+        }
+        case 'COUNTER_MINUS': {
+            return {
+                counter: state.counter - 1
+            }
+        }
+        default: {
+            return state
+        }
+    }
+}
 
 const Counter = () => {
-    const [counter, setCounter] = useState(0);
+    const [state, dispatch] = useReducer(reducer, {counter: 0});
 
     const increment = () => {
-        setCounter(counter + 1)
+        const action = {
+            type: 'COUNTER_PLUS'
+        }
+        dispatch(action);
     }
 
     const decrement = () => {
-        setCounter(counter - 1)
+        const action = {
+            type: 'COUNTER_MINUS'
+        }
+        dispatch(action);
     }
     
     return (
         <>
-            <h1>{counter}</h1>
+            <h1>{state.counter}</h1>
             <button onClick={increment}>+</button>
             <button onClick={decrement}>-</button>
         </>
